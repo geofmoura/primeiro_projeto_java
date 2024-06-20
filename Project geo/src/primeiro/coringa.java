12cd5f6a79ef898f838b8850e100a3e43d2037bd
Geovana Moura <148665372+geofmoura@users.noreply.github.com>
2024-06-20 19:29:44 -0300

    Práticas

--- Project geo/src/primeiro/coringa.java
+++ Project geo/src/primeiro/coringa.java
@@ -2,7 +2,36 @@ package primeiro;
 
 public class coringa { public static void main(String[] args) {
 	
-	System.out.println("Hello World!");
+	System.out.println("Hello World! Apenas algumas práticas..");
+	
+	System.out.println("XXXXXX");
+	System.out.println("X    X");
+	System.out.println("X    X");
+	System.out.println("X    X");
+	System.out.println("XXXXXX");
+	
+	//ainda estou com dificuldades para quebra de linha
+	
+	System.out.println("      X");
+	System.out.println("     XXX");
+	System.out.println("    XXXXX");
+	System.out.println("   XXXXXXX");
+	System.out.println("  XXXXXXXXX");
+	System.out.println(" XXXXXXXXXXX");
+	System.out.println("      XX    ");
+	
+	System.out.println("Exibição de tabela abaixo:");
+	System.out.println("ALUNO(A)   NOTA");
+	System.out.println("=======    ====");
+	System.out.println("TEREZA     9.5");
+	System.out.println("MARIO       8");
+	System.out.println("LUCAS      5.6");
+	System.out.println("GEOVANA    9.9");
+	
+	System.out.println("L");
+	System.out.println("L");
+	System.out.println("L");
+	System.out.println("LLLLL");
 	
 }
 
